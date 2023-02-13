26489da8cc92bc997514b9d355f7a1e338537ba1
Muhammad ILYAS <ilyas.muhammad@bufdir.no>
2023-02-13 13:35:32 +0100

    Removed unused code from client-app project

--- client-app/src/constants/SampleData.ts
+++ client-app/src/constants/SampleData.ts
@@ -1,26 +1,3 @@
 export const familyMember = [
-    {
-      key: 'Nadia',
-      text: 'Nadia',
-      value: 'Nadia',
-      image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
-    },
-    {
-      key: 'Imaan',
-      text: 'Imaan',
-      value: 'Imaan',
-      image: { avatar: true, src: '/images/avatar/small/elliot.jpg' },
-    },
-    {
-      key: 'Amelia',
-      text: 'Amelia',
-      value: 'Amelia',
-      image: { avatar: true, src: '/images/avatar/small/stevie.jpg' },
-    },
-    {
-      key: 'Ilyas',
-      text: 'Ilyas',
-      value: 'Ilyas',
-      image: { avatar: true, src: '/images/avatar/small/christian.jpg' },
-    }
+  
 ]
